707c3784f6069554d3ba0d7bcc8ea2d6df5a958b
Pangtang <anthoy.blanchflower@btinternet.com>
2018-06-02 19:31:16 +0100

    Add colour gradient to active elements according to age. Add age to rules.

--- __main__.py
+++ __main__.py
@@ -3,13 +3,13 @@ import pygame
 from pygame.locals import *
 
 # define dimensions of world space
-world_x_limit = 64
-world_y_limit = 64
+world_x_limit = 100
+world_y_limit = 100
 # define shape of world space
 world_size = (world_x_limit, world_y_limit)
 # define ratio of world space size to display size
 display_sc_y = 10
-display_sc_x = 20
+display_sc_x = 10
 # define shape of display
 display_size = (world_x_limit * display_sc_x, world_y_limit * display_sc_y)
 # populate world space with active element noise at ratio 1:10
@@ -18,6 +18,16 @@ world_space = np.random.binomial(1, 0.05, size=world_size)
 new_world_space = np.zeros(world_size)
 # define vectors surrounding each element to which rule checks are applied
 vectors = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
+# define color gradient of elements
+colors = [(38, 230, 0),
+          (34, 204, 0),
+          (30, 179, 0),
+          (26, 153, 0),
+          (21, 128, 0),
+          (17, 102, 0),
+          (13, 77, 0),
+          (9, 51, 0),
+          (4, 26, 0)]
 # initialise pygame instance
 pygame.init()
 # set display caption
@@ -37,90 +47,75 @@ def report_adjacent_cells(x, y):
         # check adjacent cell is in world space
         if (0 < x_vector <= world_x_limit - 1) and (0 < y_vector <= world_x_limit - 1):
             # check adjacent cell is active
-            if world_space[x_vector, y_vector] == 1:
+            if world_space[x_vector, y_vector] > 0:
                 # increment volume of adjacent active cells
                 adjacent_active_cells += 1
     return adjacent_active_cells
 
 
 def report_cell_outcome(x, y):
-
     adjacent_cells = report_adjacent_cells(x, y)
 
     cell_outcome = world_space[x, y]
 
-    # Each dead cell adjacent to
-    # exactly three live neighbors
-    # will become live in the next generation
+    # Each inactive element adjacent to
+    # exactly three active neighbors
+    # will become active in the next generation
     if adjacent_cells == 3 and world_space[x, y] == 0:
         cell_outcome = 1
     else:
-        if world_space[x, y] == 1:
-            # Each live cell with one or
-            # fewer live neighbors will die
+        if world_space[x, y] > 0:
+            # Each active element with one or
+            # fewer active neighbors will become inactive
             # in the next generation
             if adjacent_cells < 1:
                 cell_outcome = 0
             else:
-                # Each live cell with either two or
-                # three live neighbors will remain
-                # alive for the next generation.
+                # Each active element with either two or
+                # three active neighbors will remain
+                # active for the next generation and will
+                # increase in age
                 if adjacent_cells < 4:
-                    cell_outcome = 1
+                    if cell_outcome < 9:
+                        cell_outcome += 1
                 else:
-                    # Each live cell with four or more
-                    # live neighbors will die in the
+                    # Each active element with four or more
+                    # active neighbors will become inactive in the
                     # next generation
                     cell_outcome = 0
+
     return cell_outcome
 
 
 def main():
-
     running = True
 
     while running:
-        
-        touch = False
-        
+
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
-            else:
-                if event.type == pygame.MOUSEMOTION:
-                    m_x, m_y = event.pos
-                    c_x = int(round(m_x / display_sc_x, 0))
-                    c_y = int(round(m_y / display_sc_y, 0))
-                    new_seed = [[1, 1, 0, 0],  [1, 1, 0, 0],  [0, 0, 1, 1],  [0, 0, 1, 1]]
-                    # print ( "m_x: " + str(m_x) + " , m_y: " + str(m_y))
-                    # print ( "c_x: " + str(c_x) + " , c_y: " + str(c_y))
-
-                    touch = True
 
         for x, y in np.ndindex(world_space.shape):
             cell_outcome = report_cell_outcome(x, y)
             rect_size_x = display_sc_x - 1
             rect_size_y = display_sc_y - 1
-            rect_x =  x * display_sc_x
+            rect_x = x * display_sc_x
             rect_xa = rect_x - rect_size_x
-            rect_y =  y * display_sc_y
+            rect_y = y * display_sc_y
             rect_ya = rect_y - rect_size_y
             rect_shape = [rect_xa, rect_ya, rect_x, rect_y]
-            if cell_outcome == 1:
-                pygame.draw.rect(world_screen, (153, 255, 51), rect_shape)
+            if cell_outcome > 0:
+                pygame.draw.rect(world_screen, colors[cell_outcome - 1], rect_shape)
             else:
                 pygame.draw.rect(world_screen, (0, 0, 0), rect_shape)
-        
-            new_world_space[x, y] = cell_outcome
 
-        if touch and (c_x > 0 and c_x < (world_x_limit - 5)) and (c_y > 0 and c_y < (world_y_limit - 5)):
-            # new_world_space[c_x, c_y] = 1
-            new_world_space[c_x:c_x + 4, c_y: c_y + 4] = new_seed
+            new_world_space[x, y] = cell_outcome
 
         world_space[:] = new_world_space
 
         # render screen
         pygame.display.flip()
-        
+
 
 main()
