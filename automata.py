913585893bc25b07ef9a7715965416a63bad8b33
Pang Tang <anthonyblanchflower@btinternet.com>
2018-06-04 20:32:44 +0100

    alpha release

--- automata.py
+++ automata.py
@@ -9,14 +9,11 @@ world_y_limit = 96
 # define shape of world space
 world_size = (world_x_limit, world_y_limit)
 # define ratio of world space size to display size
-display_sc_y = 8
-display_sc_x = 8
+display_sc_y = 5
+display_sc_x = 5
 # define shape of display
 display_size = (world_x_limit * display_sc_x, world_y_limit * display_sc_y)
-# populate world space with active element noise at ratio 1
-## random seed noise:
-## world_space = np.random.binomial(1, 0.05, size=world_size)
-## R Pentomino seed:
+# create R Pentomino seed
 world_space = np.zeros(world_size)
 c_x = int(round((world_x_limit / 2) - 1))
 c_y = int(round((world_y_limit / 2) - 1))
@@ -26,20 +23,14 @@ world_space[c_x:c_x + 3, c_y: c_y + 3] = new_seed
 new_world_space = np.zeros(world_size)
 # define vectors surrounding each element to which rule checks are applied
 vectors = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
-# define color gradient of elements
-colors = [(38, 230, 0),
-          (34, 204, 0),
-          (30, 179, 0),
-          (26, 153, 0),
-          (21, 128, 0),
-          (17, 102, 0),
-          (13, 77, 0),
-          (9, 51, 0),
-          (4, 26, 0)]
 # initialise pygame instance
 pygame.init()
 # set display caption
 pygame.display.set_caption('automata')
+# initialise pygame font
+pygame.font.init()
+# set game font
+gamefont = pygame.font.SysFont('Ariel', 24)
 # set display canvas
 world_screen = pygame.display.set_mode(display_size)
 
@@ -63,56 +54,25 @@ def report_adjacent_cells(x, y):
 
 def report_cell_outcome(x, y):
     adjacent_cells = report_adjacent_cells(x, y)
-
     cell_outcome = world_space[x, y]
 
-    # each inactive element adjacent to
-    # exactly three active neighbors
-    # will become active in the next generation
-    #if adjacent_cells == 3 and world_space[x, y] == 0:
-    #    cell_outcome = 1
-    #else:
-    #    if world_space[x, y] > 0:
-            # each active element with one or
-            # fewer active neighbors will become inactive
-            # in the next generation
-    #        if adjacent_cells < 1:
-    #            cell_outcome = 0
-    #        else:
-                # each active element with either two or
-                # three active neighbors will remain
-                # active for the next generation and will
-                # increase in age
-    #            if adjacent_cells < 4:
-    #                if cell_outcome < 9:
-    #                    cell_outcome += 1
-    #            else:
-                    # each active element with four or more
-                    # active neighbors will become inactive in the
-                    # next generation
-    #                cell_outcome = 0
-
-    if adjacent_cells == 2:
-        if 0 < cell_outcome < 9:
-            cell_outcome += 1
+    if cell_outcome == 1:
+        if adjacent_cells < 2 or adjacent_cells > 3:
+            cell_outcome = 0
+        else:
+            cell_outcome = 1
     else:
         if adjacent_cells == 3:
-            if 0 < cell_outcome < 9:
-                cell_outcome += 1
-            else:
-                if cell_outcome == 0:
-                    cell_outcome = 1
-        else:
-            cell_outcome == 0
+            cell_outcome = 1
 
     return cell_outcome
 
 
 def main():
     running = True
+    gen_count = 0
 
     while running:
-
         # capture escape event
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
@@ -129,7 +89,7 @@ def main():
             rect_ya = rect_y - rect_size_y
             rect_shape = [rect_xa, rect_ya, rect_x, rect_y]
             if cell_outcome > 0:
-                pygame.draw.rect(world_screen, colors[int(cell_outcome - 1)], rect_shape)
+                pygame.draw.rect(world_screen, (17, 102, 0), rect_shape)
             else:
                 pygame.draw.rect(world_screen, (0, 0, 0), rect_shape)
 
@@ -137,6 +97,10 @@ def main():
 
         # load next generation of elements to world space
         world_space[:] = new_world_space
+
+        gen_count += 1
+        text_surface = gamefont.render("generations: " + str(gen_count), False, (17, 102, 0))
+        world_screen.blit(text_surface, (5, 5))
 
         # apply post processing
         rgbarray = surfarray.array3d(world_screen)
@@ -147,7 +111,7 @@ def main():
         soften[:-1, :] += rgbarray[1:, :] * factor
         soften[:, 1:] += rgbarray[:, :-1] * factor
         soften[:, :-1] += rgbarray[:, 1:] * factor
-        # soften //= 33
+        soften //= 16
         screen = pygame.display.set_mode(soften.shape[:2], 0, 32)
         surfarray.blit_array(screen, soften)
 
