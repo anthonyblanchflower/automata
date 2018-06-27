6823040d571d47fc0b7a85a516c73931c218ae73
Pangtang <anthonyblanchflower@btinternet.com>
2018-06-27 12:43:33 +0100

    Add low density noise as a seed

--- automata/automata.py
+++ automata/automata.py
@@ -74,11 +74,15 @@ def inject_seed(seed_json):
         seed_dict = json.load(f)
     seed_array = seed_dict['seed_array']
     seed_name = seed_dict['seed_name']
-    size_x = len(seed_array[0])
-    size_y = len(seed_array)
-    c_x = int(round((world_x_limit / 2) - 1))
-    c_y = int(round((world_y_limit / 2) - 1))
-    world_space[c_x:c_x + size_x, c_y: c_y + size_y] = seed_array
+    if seed_name == 'Low Density Noise':
+        # populate world space with active element noise at ratio 1:10
+        world_space[:] = np.random.binomial(1, 0.05, size=world_size)
+    else:
+        size_x = len(seed_array[0])
+        size_y = len(seed_array)
+        c_x = int(round((world_x_limit / 2) - 1))
+        c_y = int(round((world_y_limit / 2) - 1))
+        world_space[c_x:c_x + size_x, c_y: c_y + size_y] = seed_array
     return seed_name
 
 
